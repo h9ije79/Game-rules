5de46e9e3e8b531d2db398afd4807d96f94b7bee
h9ije79 <yvgoncharuk23@gmail.com>
2025-03-22 17:18:51 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,27 +6,4 @@ const adminsTab = document.getElementById('admins');
 continueBtn.addEventListener('click', () => {
     document.querySelector('.welcome-screen').classList.add('hidden');
     mainContent.classList.remove('hidden');
-    showTab('rules');
-});
-
-function showTab(tab) {
-    rulesTab.classList.add('hidden');
-    adminsTab.classList.add('hidden');
-    if (tab === 'rules') {
-        rulesTab.classList.remove('hidden');
-    } else if (tab === 'admins') {
-        adminsTab.classList.remove('hidden');
-    }
-}
-
-function showSubTab(subTab) {
-    const gameRules = document.getElementById('game-rules');
-    const discordRules = document.getElementById('discord-rules');
-    gameRules.classList.add('hidden');
-    discordRules.classList.add('hidden');
-    if (subTab === 'game-rules') {
-        gameRules.classList.remove('hidden');
-    } else if (subTab === 'discord-rules') {
-        discordRules.classList.remove('hidden');
-    }
-}
+    showTab('rules
